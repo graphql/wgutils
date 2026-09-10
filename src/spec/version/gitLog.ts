a4e36eff67bfcb413760fe654eae33d553fcb11b
Benjie Gillam <benjie@jemjie.com>
2026-09-10 11:44:00 +0100

    Don't escape braces around email addresses/etc

--- src/spec/version/gitLog.ts
+++ src/spec/version/gitLog.ts
@@ -4,19 +4,19 @@ import { execGit } from "../../git";
 const DIVIDER = "§FIELD_DIVIDE§";
 
 function markdownTableEscape(str: string) {
-  return str.replace(
-    /[&<>|]|\n|\r\n?/g,
-    (t) =>
-      ({
-        "&": "&amp;",
-        "<": "&lt;",
-        ">": "&gt;",
-        "|": "&#124;",
-        "\n": "<br/>",
-        "\r\n": "<br/>",
-        "\r": "<br/>",
-      })[t] ?? t,
-  );
+  return str
+    .replace(
+      /[&|]|\n|\r\n?/g,
+      (t) =>
+        ({
+          "&": "&amp;",
+          "|": "&#124;",
+          "\n": "<br/>",
+          "\r\n": "<br/>",
+          "\r": "<br/>",
+        })[t] ?? t,
+    )
+    .replace(/<([a-zA-Z]+(?:[ >]|\/>))/g, "&lt;$1");
 }
 
 export async function gitLog(previousTag: string, HEAD: string, path: string) {
